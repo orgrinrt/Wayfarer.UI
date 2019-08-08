76bbf350e269d3027833c4d9ab631d78ff0a6752
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-08 13:34:32 +0300

    Initial logic for the UI core plugin

--- UICorePlugin.cs
+++ UICorePlugin.cs
@@ -1,6 +1,48 @@
-﻿namespace Wayfarer.UI
+﻿#if TOOLS
+
+using Godot;
+using Wayfarer.ModuleSystem;
+
+namespace Wayfarer.UI
 {
-    public class UICorePlugin
+    [Tool]
+    public class UICorePlugin : WayfarerModule
     {
+        public override void _EnterTreeSafe()
+        {
+            AddCustomTypes();
+        }
+
+        public override void _ExitTreeSafe()
+        {
+            RemoveCustomTypes();
+        }
+
+        private void AddCustomTypes()
+        {
+            Texture tempIcon = GD.Load<Texture>("res://Addons/Wayfarer.Core/Assets/Icons/manager.png");
+            Texture defaultIcon = GD.Load<Texture>("res://icon.png");
+            
+            Script organizableContainer = GD.Load<Script>("res://Addons/Wayfarer.UI/Controls/OrganizableContainer/OrganizableContainer.cs");
+            if (organizableContainer != null)
+            {
+                AddCustomType("OrganizableContainer", "Control", organizableContainer, tempIcon ?? defaultIcon);
+            }
+            
+            Script organizableItem = GD.Load<Script>("res://Addons/Wayfarer.UI/Controls/OrganizableItem/OrganizableItem.cs");
+            if (organizableItem != null)
+            {
+                AddCustomType("OrganizableItem", "Control", organizableItem, tempIcon ?? defaultIcon);
+            }
+        }
+
+        private void RemoveCustomTypes()
+        {
+            RemoveCustomType("OrganizableContainer");
+            RemoveCustomType("OrganizableItem");
+        }
+        
     }
 }
+
+#endif
