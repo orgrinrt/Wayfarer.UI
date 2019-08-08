a3e618e0aae51196f64dd759e2eb5608985db4e7
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-08 13:34:04 +0300

    Interface for draggable controls

---
+++ Controls/IDraggableControl.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using Wayfarer.Core.Systems.Managers;
+
+namespace Wayfarer.UI.Controls
+{
+    public interface IDraggableControl
+    {
+        bool IsDragged { get; }
+        MouseManager MouseManager { get;  }
+        void StartDrag();
+        void EndDrag(Node node);
+        MouseManager GetMouseManager();
+    }
+}
